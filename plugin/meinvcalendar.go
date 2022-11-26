6fefcb68f04660a68c003c17714f18e79336eef3
huaixwuyu <44761149+huaixwuyu@users.noreply.github.com>
2022-11-26 18:43:14 +0800

    meinvcalendar
    meinvcalendar

---
+++ plugin/meinvcalendar.go
@@ -0,0 +1,29 @@
+// Package moyucalendar 摸鱼人日历
+package 美女日历
+
+import (
+	"github.com/FloatTech/floatbox/web"
+	ctrl "github.com/FloatTech/zbpctrl"
+	"github.com/FloatTech/zbputils/control"
+	zero "github.com/wdvxdr1123/ZeroBot"
+	"github.com/wdvxdr1123/ZeroBot/message"
+)
+
+func init() {
+	control.Register("meinvcalendar", &ctrl.Options[*zero.Ctx]{
+		DisableOnDefault: true,
+		Brief:            "美女日历",
+		Help: "- /启用 meinvcalendar\n" +
+			"- /禁用 meinvcalendar\n" +
+			"- 记录在\"00 19 * * *\"触发的指令\n" +
+			"   - 美女日历",
+	}).OnFullMatch("美女日历").SetBlock(true).
+		Handle(func(ctx *zero.Ctx) {
+			data, err := web.GetData("https://api.vvhan.com/api/mobil.girl")
+			if err != nil {
+				ctx.SendChain(message.Text("ERROR: ", err))
+				return
+			}
+			ctx.SendChain(message.ImageBytes(data))
+		})
+}
