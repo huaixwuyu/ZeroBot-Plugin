3c320af4513bf1f6334b479fe53c2aa27adc1bf9
huaixwuyu <44761149+huaixwuyu@users.noreply.github.com>
2022-11-26 21:46:31 +0800

    Update main.go

--- main.go
+++ main.go
@@ -139,7 +139,7 @@ import (
 	_ "github.com/huaixwuyu/ZeroBot-Plugin/plugin/ymgal"         // 月幕galgame
 	_ "github.com/huaixwuyu/ZeroBot-Plugin/plugin/meinvcalendar" // 美女日历
 	_ "github.com/huaixwuyu/ZeroBot-Plugin/plugin/dailycalendar" // 日历
-	_ "github.com/huaixwuyu/ZeroBot-Plugin/plugin/girl"          // 美女
+	_ "github.com/huaixwuyu/ZeroBot-Plugin/plugin/girl" // 美女
 	_ "github.com/huaixwuyu/ZeroBot-Plugin/plugin/wtf"            // 鬼东西
 
 	//                               ^^^^                               //
@@ -178,7 +178,7 @@ import (
 	//                                                                  //
 	//                                                                  //
 	// -----------------------以下为内置依赖，勿动------------------------ //
-	"github.com/huaixwuyu/floatbox/process"
+	"github.com/FloatTech/floatbox/process"
 	"github.com/sirupsen/logrus"
 	zero "github.com/wdvxdr1123/ZeroBot"
 	"github.com/wdvxdr1123/ZeroBot/driver"
@@ -204,7 +204,7 @@ func init() {
 	// 直接写死 URL 时，请更改下面第二个参数
 	url := flag.String("u", "ws://127.0.0.1:6700", "Set Url of WSClient.")
 	// 默认昵称
-	adana := flag.String("n", "椛椛酱", "Set default nickname.")
+	adana := flag.String("n", "椛椛", "Set default nickname.")
 	prefix := flag.String("p", "/", "Set command prefix.")
 	runcfg := flag.String("c", "", "Run from config file.")
 	save := flag.String("s", "", "Save default config to file and exit.")
