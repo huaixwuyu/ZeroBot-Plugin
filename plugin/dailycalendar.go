f532d3a6132f131a1c1543994b09dddd370e197c
huaixwuyu <44761149+huaixwuyu@users.noreply.github.com>
2022-11-26 18:46:27 +0800

    dailycalendar
    dailycalendar

---
+++ plugin/dailycalendar.go
@@ -0,0 +1,29 @@
+// Package dailycalendar 日历
+package 日历
+
+import (
+	"github.com/FloatTech/floatbox/web"
+	ctrl "github.com/FloatTech/zbpctrl"
+	"github.com/FloatTech/zbputils/control"
+	zero "github.com/wdvxdr1123/ZeroBot"
+	"github.com/wdvxdr1123/ZeroBot/message"
+)
+
+func init() {
+	control.Register("meinvcalendar", &ctrl.Options[*zero.Ctx]{
+		DisableOnDefault: true,
+		Brief:            "日历",
+		Help: "- /启用 dailycalendar\n" +
+			"- /禁用 dailycalendar\n" +
+			"- 记录在\"00 7 * * *\"触发的指令\n" +
+			"   - 日历",
+	}).OnFullMatch("日历").SetBlock(true).
+		Handle(func(ctx *zero.Ctx) {
+			data, err := web.GetData("https://api.vvhan.com/api/60s")
+			if err != nil {
+				ctx.SendChain(message.Text("ERROR: ", err))
+				return
+			}
+			ctx.SendChain(message.ImageBytes(data))
+		})
+}
